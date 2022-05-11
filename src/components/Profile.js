f1694357eb4eb3d2a1679a8b996a0eb41992267a
Chris <orj.tsuk@gmail.com>
2022-05-11 08:37:47 -0400

    working on adding the ability to upload a profile image

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,155 @@
+import './Profile.css'
+import { useUserAuth } from '../contexts/UserAuthContext'
+import { useEffect, useState } from 'react'
+import { doc, getDoc } from "firebase/firestore";
+import { db, storage } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL, listAll } from "firebase/storage";
+import newUser from '../images/newUser.jpg'
+
 const Profile = () => {
-    return(
-        <h1>Hello Profile</h1>
-    )
+
+    const { authUser } = useUserAuth()
+    const [user, setUser] = useState()
+    const [upload, showUpload] = useState()
+    const [uploadFile, setUploadFile] = useState()
+    const [imgPath, setImgPath] = useState()
+
+    useEffect(() => {
+        if (authUser?.uid != null) {
+            getUserData()
+            getUserImage()
+        } else {
+
+        }
+    }, [authUser])
+
+    const getUserImage = () => {
+        const listRef = ref(storage, `/images/users/${authUser.uid}`)
+        // Find all the prefixes and items.
+        listAll(listRef)
+            .then((res) => {
+                res.prefixes.forEach((folderRef) => {
+                    console.log(`folderRef `, folderRef)
+                });
+                res.items.forEach((itemRef) => {
+                   setUser((old) => {
+                       return({
+                           ...old,
+                           profile_image: `/users/${authUser.uid}/${itemRef.name}`
+                       })
+                   })
+                });
+            }).catch((error) => {
+                // Uh-oh, an error occurred!
+            });
+    }
+
+    const getUserData = async () => {
+        if (authUser != null) {
+            const userRef = doc(db, 'users', authUser.uid)
+            const docSnap = await getDoc(userRef)
+            if (docSnap.exists()) {
+                setUser(docSnap.data());
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such user!");
+            }
+        }
+    }
+
+    const handleChange = (event) => {
+        setUploadFile(event.target.files[0])
+    }
+
+    const uploadData = async () => {
+        //maybe import this function from outside...?
+
+        const metadata = {
+            contentType: 'image/jpeg'
+        };
+        const storageRef = ref(storage, `images/users/${user.id}/` + uploadFile.name)
+        const uploadTask = uploadBytesResumable(storageRef, uploadFile, metadata)
+
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+                switch (snapshot.state) {
+                    case 'paused':
+                        console.log('Upload is paused');
+                        break;
+                    case 'running':
+                        console.log('Upload is running');
+                        break;
+                }
+            },
+            (error) => {
+                // A full list of error codes is available at
+                // https://firebase.google.com/docs/storage/web/handle-errors
+                switch (error.code) {
+                    case 'storage/unauthorized':
+                        // User doesn't have permission to access the object
+                        break;
+                    case 'storage/canceled':
+                        // User canceled the upload
+                        break;
+
+                    // ...
+
+                    case 'storage/unknown':
+                        // Unknown error occurred, inspect error.serverResponse
+                        break;
+                }
+            },
+            () => {
+                // Upload completed successfully, now we can get the download URL
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    console.log('File available at', downloadURL);
+                    setImgPath(downloadURL)
+                });
+            }
+        );
+        showUploadModal(false)
+    }
+
+    const showUploadModal = () => {
+        showUpload(true)
+    }
+    const hideUpload = () => {
+        // showUpload(false)
+    }
+
+    const scriptElements = user?.posts.map((item, i) => {
+        return (
+            <div key={i} className='userScripts--container'>
+                <h3 className='userScripts--user'>{item.user}</h3>
+                <h4 className='userScripts--content'>{item.post}</h4>
+            </div>
+        )
+    }).reverse()
+
+    if (user) {
+        return (
+            <div onClick={hideUpload} className='profile--content--container'>
+                {upload &&
+                    <div className='profile--upload'>
+                        <h4>Select a profile image to upload:</h4>
+                        <input onChange={handleChange} type='file' name='file' />
+                        <button onClick={uploadData}>Submit</button>
+                    </div>}
+                <div className='profile--container'>
+                    {user.profile_image ? <img onClick={showUploadModal} className='profile--image' src={user.profile_image} /> :
+                        <img onClick={showUploadModal} className='profile--image' src={newUser} />}
+                    <h1 className='profile--username'>{user.username}</h1>
+                </div>
+                <div>
+                    <h1 className='userScripts--title'>Your Scripts</h1>
+                    {scriptElements}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Profile
--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from 'firebase/auth'
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,7 +22,8 @@ const firebaseConfig = {
 const firebase = initializeApp(firebaseConfig);
 const db = getFirestore(firebase)
 const auth = getAuth(firebase)
+const storage = getStorage();
 // const auth = firebase.auth();
 // const storage = firebase.storage();
 
-export { db, auth };
+export { db, auth, storage };
