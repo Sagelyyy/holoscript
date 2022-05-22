34f2db7d822580422d0582d2573e252b34cb9cc9
Chris <orj.tsuk@gmail.com>
2022-05-21 20:23:43 -0400

    fixed small bug checking the usernames in like count with capitalization.

--- src/components/PostFeed.js
+++ src/components/PostFeed.js
@@ -1,7 +1,7 @@
 import './PostFeed.css'
 import { useUserAuth } from '../contexts/UserAuthContext'
 import { useEffect, useState } from 'react'
-import { getDoc, doc, onSnapshot, updateDoc, query, where, collection, getDocs, arrayUnion, QuerySnapshot, increment, } from 'firebase/firestore'
+import { getDoc, doc, onSnapshot, updateDoc, query, collection, getDocs, arrayUnion, increment, arrayRemove, } from 'firebase/firestore'
 import { db } from '../firebase'
 import MessageModal from './MessageModal'
 
@@ -17,10 +17,6 @@ const PostFeed = () => {
     useEffect(() => {
         if (authUser?.uid != null) {
             getUserData()
-            // const unsub = onSnapshot(doc(db, "allScripts"), (doc) => {
-            //     console.log(doc)
-            // });
-            // return unsub
             const q = query(collection(db, 'allScripts'))
             const unsub = onSnapshot(q, (querySnapshot) => {
                 const posts = []
@@ -29,6 +25,7 @@ const PostFeed = () => {
                 })
                 setPostData(posts)
             })
+            return unsub
         }
         setShowMessageModal(false)
     }, [authUser])
@@ -56,10 +53,9 @@ const PostFeed = () => {
     }
 
     const handleLike = async (postId) => {
-        const selectedPost = []
         postData.map(async (item, i) => {
             if (item.id === postId) {
-                if(!checkLikedPosts(item.liked_by, user)){
+                if (!checkLikedPosts(item.liked_by, user)) {
                     const q = query(collection(db, 'allScripts'))
                     const querySnapshot = await getDocs(q)
                     querySnapshot.forEach((scr) => {
@@ -69,14 +65,27 @@ const PostFeed = () => {
                                 likes: increment(1)
                             })
                             updateDoc(postsRef, {
-                                liked_by: arrayUnion(user.username)
+                                liked_by: arrayUnion(user.username.toLowerCase())
+                            })
+                        }
+                    })
+                } else {
+                    const q = query(collection(db, 'allScripts'))
+                    const querySnapshot = await getDocs(q)
+                    querySnapshot.forEach((scr) => {
+                        if (scr.data().id === postId) {
+                            const postsRef = doc(db, 'allScripts', scr.id)
+                            updateDoc(postsRef, {
+                                likes: increment(-1)
+                            })
+                            updateDoc(postsRef, {
+                                liked_by: arrayRemove(user.username.toLowerCase())
                             })
                         }
                     })
                 }
             }
         })
-
     }
 
     const postElements = postData?.map((item, i) => {
