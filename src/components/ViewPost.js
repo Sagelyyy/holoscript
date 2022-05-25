59989fb7ae3e9cdbd1901a57e49b87840894dfd4
Chris <orj.tsuk@gmail.com>
2022-05-24 22:13:27 -0400

    Getting a rough copy of viewpost working. Liking is a little buggy right now.

--- src/components/ViewPost.js
+++ src/components/ViewPost.js
@@ -1,12 +1,141 @@
 import { useParams } from "react-router-dom"
+import { getDoc, doc, onSnapshot, updateDoc, query, collection, getDocs, arrayUnion, increment, arrayRemove, where } from 'firebase/firestore'
+import { db } from "../firebase";
+import { useState, useEffect } from "react";
+import { useUserAuth } from "../contexts/UserAuthContext";
+import { Link } from "react-router-dom";
 
 const ViewPost = () => {
-    const {id} = useParams()
-    console.log(id)
+
+    const { authUser } = useUserAuth()
+    const { id } = useParams()
+    const [postData, setPostData] = useState([])
+    const [user, setUser] = useState()
+    const [messageSelection, setMessageSelection] = useState()
+    const [showMessageModal, setShowMessageModal] = useState()
+
+    console.log(postData)
+
+    useEffect(() => {
+        if (authUser?.uid != null) {
+            getUserData()
+            getPostData(id)
+        }
+        setUser()
+    }, [authUser])
+
+    const handleMessage = (username) => {
+        setMessageSelection(username)
+        setShowMessageModal((old) => !old)
+    }
+
+    const didUserLike = (arr, user) => {
+        return arr.some(arrVal => user.username.toLowerCase() === arrVal)
+    }
+
+    const handleLike = async (postId) => {
+        postData.map(async (item, i) => {
+            if (item.id === postId) {
+                if (!didUserLike(item.liked_by, user)) {
+                    const q = query(collection(db, 'allScripts'))
+                    const querySnapshot = await getDocs(q)
+                    querySnapshot.forEach((scr) => {
+                        if (scr.data().id === postId) {
+                            const postsRef = doc(db, 'allScripts', scr.id)
+                            updateDoc(postsRef, {
+                                likes: increment(1)
+                            })
+                            updateDoc(postsRef, {
+                                liked_by: arrayUnion(user.username.toLowerCase())
+                            })
+                        }
+                    })
+                } else {
+                    const q = query(collection(db, 'allScripts'))
+                    const querySnapshot = await getDocs(q)
+                    querySnapshot.forEach((scr) => {
+                        if (scr.data().id === postId) {
+                            const postsRef = doc(db, 'allScripts', scr.id)
+                            updateDoc(postsRef, {
+                                likes: increment(-1)
+                            })
+                            updateDoc(postsRef, {
+                                liked_by: arrayRemove(user.username.toLowerCase())
+                            })
+                        }
+                    })
+                }
+            }
+        })
+    }
+
+    const getUserData = async () => {
+        if (authUser != null) {
+            const userRef = doc(db, 'users', authUser?.uid)
+            const docSnap = await getDoc(userRef)
+            if (docSnap.exists()) {
+                setUser(docSnap.data());
+            } else {
+                console.log("No such user!");
+            }
+        }
+    }
+
+    const getPostData = async (id) => {
+        const data = []
+        const q = query(collection(db, "allScripts"), where("id", "==", id));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+           data.push(doc.data())
+        });
+        setPostData([...data])
+    }
+
+    const postElements = postData?.map((item, i) => {
+        if (item.liked_by.some(arrVal => user?.username === arrVal)) {
+            return (
+                <div key={i} className='postFeed--container'>
+                    <div className='postFeed--user--container'>
+                        <img className="postFeed--user--avatar" src={item.user_profile_image} />
+                        <h3 onClick={() => handleMessage(item.user)} className='postFeed--user--username'>{item.user}</h3>
+                    </div>
+                    <Link to={'post/' + item.id}>
+                        <div>
+                            <h4 className='postFeed--content'>{item.post}</h4>
+                        </div>
+                        </Link>
+                    <div className='postFeed--buttons'>
+                        <span onClick={() => handleLike(item.id)} className="material-icons postButton liked">
+                            favorite{item.likes > 0 ? <span className='postFeed--likes liked'>{item.likes}</span> : null}</span>
+                        <span className="material-icons postButton">forum</span>
+                    </div>
+                </div>
+            )
+        } else {
+            return (
+                <div key={i} className='postFeed--container'>
+                    <div className='postFeed--user--container'>
+                        <img className="postFeed--user--avatar" src={item.user_profile_image} />
+                        <h3 onClick={() => handleMessage(item.user)} className='postFeed--user--username'>{item.user}</h3>
+                    </div>
+                    <Link to={'post/' + item.id}>
+                        <div>
+                            <h4 className='postFeed--content'>{item.post}</h4>
+                        </div>
+                        </Link>
+                    <div className='postFeed--buttons'>
+                        <span onClick={() => handleLike(item.id)} className="material-icons postButton">
+                            favorite{item.likes > 0 ? <span className='postFeed--likes'>{item.likes}</span> : null}</span>
+                        <span className="material-icons postButton">forum</span>
+                    </div>
+                </div>
+            )
+        }
+    })
 
     return (
         <div>
-          <h3>ID: {id}</h3>
+            {postElements}
         </div>
     )
 }
